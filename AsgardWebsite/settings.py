7162e48cd5a4a8f0101e2d231f196c5b23e1c82c
AnnMEldo31 <annmaryeldo31@gmail.com>
2022-09-22 13:09:07 +0530

    commit for deployment fix

--- AsgardWebsite/settings.py
+++ AsgardWebsite/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
-STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static/"),
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
